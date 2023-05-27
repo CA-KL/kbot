8de4427477a7f3a553ef32b1a80914a0fe4efc22
keleleo <keleleo1k@gmail.com>
2023-05-27 17:34:39 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageReactions,
+  ],
+});
+
+client.on('ready', () => {
+  console.log('🚀 Bot running!');
+  console.log('BATATA');
+});
+
+client.login(process.env.DISCORD_TOKEN);
